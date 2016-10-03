52aec668167dc84d18b6c7ba3c5cc14fef800217
Charnelx <portfel72536@gmail.com>
2016-10-03 10:26:17 +0300

    Delete commented string

--- xmljson/__init__.py
+++ xmljson/__init__.py
@@ -9,8 +9,6 @@ try:
 except ImportError:
     from xml.etree.cElementTree import Element, iterparse, ElementTree
 
-# from lxml.etree import Element, iterparse, ElementTree, tostring
-
 __author__ = 'S Anand'
 __email__ = 'root.node@gmail.com'
 __version__ = '0.1.7'
